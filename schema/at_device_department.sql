d82b944b3a80bf832bdffd09e757eef936d13db7
lwk <qwfys200@qq.com>
2017-08-03 19:05:04 +0800

    update schema

--- schema/at_device_department.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `at_device_department`;
-
-CREATE TABLE `at_device_department` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
-  `device_id` int(11) NOT NULL COMMENT '设备编号',
-  `department_id` int(11) NOT NULL COMMENT '部门编号',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='设备-部门表';
